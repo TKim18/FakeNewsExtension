ab9b142a8c0cf7d83989298434c9f65f01862d0a
Tristan Ang <tangtzeheng@wesleyan.edu>
2017-12-02 20:32:43 -0500

    carlo function

--- js/gdelt.js
+++ js/gdelt.js
@@ -24,3 +24,33 @@ function queryGDELT(keyword) {
     }
   xhr.send()
 }
+
+function getEntities(query) {
+  var requestpayload = {
+						  "document":{
+						    "type":"PLAIN_TEXT",
+						    "language": "EN",
+						    "content":query
+						  },
+						  "encodingType":"UTF8"
+						}
+  console.log(requestpayload)
+  var xhr = new XMLHttpRequest();
+  
+  xhr.open("POST", 'https://language.googleapis.com/v1/documents:analyzeEntitySentiment?key='+APIKEY, true);
+  xhr.setRequestHeader('content-type', 'application/json')
+
+  xhr.onreadystatechange=function()
+  {
+  	var entities = JSON.parse(xhr.responseText).entities.slice(0,5)
+  	var result = ""
+  	
+  	for (var i = 0; i < entities.length; i++) {
+  		result = result + entities[i]['name']; + ' '
+  	}
+  	console.log(result)
+
+
+  }
+  xhr.send(JSON.stringify(requestpayload))
+}
